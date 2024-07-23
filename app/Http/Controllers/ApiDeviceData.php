16c1f5f0c73bd9289332eebc2fe6b006be28a0d7
unknown <patrickmuvunyi@gmail.com>
2024-07-23 12:30:00 +0200

    resolved api

--- app/Http/Controllers/ApiDeviceData.php
+++ app/Http/Controllers/ApiDeviceData.php
@@ -4,17 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\DeviceData;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class ApiDeviceData extends Controller
 {
     public function index()
     {
+
+        if (!Auth::guard('api')->check()) {
+            return response()->json(['message' => 'You must log in to access this resource.'], 401);
+        }
+
         $data = DeviceData::all();
         return response()->json($data);
     }
 
     public function visual()
     {
+
+        // if (!Auth::check()) {
+        //     return response()->json(['message' => 'You must log in to access this resource.'], 401);
+        // }
+
+        if (!Auth::guard('api')->check()) {
+            return response()->json(['message' => 'You must log in to access this resource.'], 401);
+        }
+
         $data = DeviceData::select('DEVICE_ID', 'S_TEMP', 'S_HUM', 'A_TEMP', 'A_HUM', 'created_at')->get();
         return response()->json($data);
     }
@@ -35,11 +49,21 @@ class ApiDeviceData extends Controller
 
     public function show(DeviceData $device_data)
     {
+
+        if (!Auth::check()) {
+            return response()->json(['message' => 'You must log in to access this resource.'], 401);
+        }
+
         return response()->json($device_data);
     }
 
     public function update(Request $request, DeviceData $device_data)
     {
+
+        if (!Auth::check()) {
+            return response()->json(['message' => 'You must log in to access this resource.'], 401);
+        }
+
         $request->validate([
             'DEVICE_ID' => 'required',
             'S_TEMP' => 'required|numeric',
@@ -56,6 +80,10 @@ class ApiDeviceData extends Controller
 
     public function destroy(DeviceData $device_data)
     {
+        if (!Auth::check()) {
+            return response()->json(['message' => 'You must log in to access this resource.'], 401);
+        }
+
         $device_data->delete();
         return response()->json(['success' => 'Device data deleted successfully.'
     ]);
--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,13 @@ Route::post('/refresh', [API_AuthController::class, 'refreshToken']);
 Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/me', [API_AuthController::class, 'me']);
     Route::post('/logout', [API_AuthController::class, 'logout']);
-
-
-
+    Route::get('device-data', [ApiDeviceData::class, 'index']);
+    Route::get('device-data/visual', [ApiDeviceData::class, 'visual']);
+    Route::get('device-data/{device_data}', [ApiDeviceData::class, 'show']);
 });
-Route::get('device-data', [ApiDeviceData::class, 'index']);
 
 //API FOR DEVIDE DATA
 
-Route::get('device-data/visual', [ApiDeviceData::class, 'visual']);
 Route::post('device-data', [ApiDeviceData::class, 'store']);
-Route::get('device-data/{device_data}', [ApiDeviceData::class, 'show']);
 Route::put('device-data/{device_data}', [ApiDeviceData::class, 'update']);
 Route::delete('device-data/{device_data}', [ApiDeviceData::class, 'destroy']);
